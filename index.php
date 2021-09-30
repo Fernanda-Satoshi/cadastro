7c544e4b5139487a286dc62e192ea6ab1e3fc992
Fernanda Satoshi <91622550+Fernanda-Satoshi@users.noreply.github.com>
2021-09-29 23:15:27 -0300

    Update index.php
    Atualização

--- index.php
+++ index.php
@@ -7,89 +7,151 @@ include('conexao.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Busca</title>
+    
+    <style>
+
+*{
+    padding: 0px;
+    margin: 0px;
+}
+
+body{
+background-color: #eefff9;
+
+}
+header {
+
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+    background-color: #3fddaa;
+    color: white;
+    font-family: Cambria, arial;
+
+
+}
+
+section {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 80vh;
+
+}
+
+
+form {
+    font-family: arial;
+
+}
+
+form div{
+    display: flex;
+    flex-direction: collumn; 
+    margin-bottom: 30px;
+
+}
+
+form textarea{
+outline: unset;
+padding: 15px;
+width: 250px;
+border: 1px solid #3fddaa;
+border-radius: 20px;
+background-color: #3fddaa;
+
+}
+
+form input{
+outline: unset;
+padding: 15px;
+width: 250px;
+border: 1px solid #3fddaa;
+border-radius: 20px;
+background-color: #fff;
+
+}
+form textarea:focus {
+
+background-color: #fff;
+
+
+ }
+
+ form input:focus {
+
+background-color: #baf7e3;
+
+
+ }
+
+
+ Form label {
+
+    margin-left: 25px;
+    font-size: 30px;
+ }
+ .city {
+  background-color: #3fddaa;
+  color: #baf7e3;
+  align-items: center;
+  justify-content: center;
+ }
+
+</style>
+
 </head>
+
 <body>
-<?php
-		if(isset($_SESSION['msg'])){
-			echo $_SESSION['msg'];
-			unset($_SESSION['msg']);
-		}
+    
+<header>
 
-
-        
-		?>
-
-<h1>Cadastrar Usuário</h1>
-
-<h2> Carregar a foto</h2>
+<img src="logo.png" width="40"> <h1>Cadastro</h1>
+    </header>
+    
+<section>
+    
+    
 <form method="POST" action="processa.php" enctype="multipart/form-data">
-	Imagem: <input name="arquivo" type="file"><br><br>				
-		<form method="POST" action="processa.php">
-			<label>Nome: </label>
-			<input type="text" name="nome" placeholder="Digite o nome completo"><br><br>			
-			<label>Bairro: </label>
-			<input type="bairro" name="bairro" placeholder="Digite o seu bairro"><br><br>
-            <label>Whatsapp: </label>
-			<input type="whatsapp" name="whatsapp" placeholder="Digite o seu whatsapp"><br><br> 
-			<input type="submit" value="Cadastrar">
-		</form>
-        </form>
-    <h1>Lista de Veículos</h1>
-    <form action="">
-        <input name="busca" value="<?php if(isset($_GET['busca'])) echo $_GET['busca']; ?>" placeholder="Digite os termos de pesquisa" type="text">
-        <button type="submit">Pesquisar</button>
-    </form>
-    <br>
-    <table width="600px" border="1">
-        <tr>
-            <th>Nome</th>
-            <th>Bairros</th>
-            <th>WhatsApp</th>
-            <th>Imagem</th>
-        </tr>
-        <?php
-        if (!isset($_GET['busca'])) {
-            ?>
-        <tr>
-            <td colspan="4">Digite algo para pesquisar...</td>
-        </tr>
-        <?php
-        } else {
-            $pesquisa = $mysqli->real_escape_string($_GET['busca']);
-            $sql_code = "SELECT * 
-                FROM usuarios 
-                WHERE nome LIKE '%$pesquisa%' 
-                OR bairro LIKE '%$pesquisa%'
-                OR whatsapp LIKE '%$pesquisa%'
-                OR nome_imagem LIKE '%$pesquisa%'";
-            $sql_query = $mysqli->query($sql_code) or die("ERRO ao consultar! " . $mysqli->error); 
-            
-            if ($sql_query->num_rows == 0) {
-                ?>
-            <tr>
-                <td colspan="4">Nenhum resultado encontrado...</td>
-            </tr>
-            <?php
-            } else {
-                while($dados = $sql_query->fetch_assoc()) {
-                    ?>
-                    <tr>
-                        <td><?php echo $dados['nome']; ?></td>
-                        <td><?php echo $dados['bairro']; ?></td>
-                        <td><?php echo $dados['whatsapp']; ?></td>
-                        <td><?php echo $dados['nome_imagem']; ?></td>
-                    </tr>
-                    <?php
-                }
-            }
-            ?>
-        <?php
-        } ?>
-    </table>
+
+    <label>Envie a sua foto</label><br><br>
+        
+        
+        <input type="file" class="custom-file-input" name="imagem" id="imagem" ><br><br>   
+
+        <input type="text" name="nome" placeholder="Digite o seu nome completo"><br><br>
+        
+        <input type="bairro" name="bairro" placeholder="Digite o seu bairro"><br><br>
+        
+        <input type="whatsapp" name="whatsapp" placeholder="Digite o seu whatsapp"><br><br>
+        <div class="city">
+        <input type="submit" value="Clique para cadastrar">
+        </div>
+
+</form>
+
+
+
+
+
+</section>
+
+<?php
+    if (isset($_SESSION['msg'])) {
+        echo $_SESSION['msg'];
+        unset($_SESSION['msg']);
+    }
+
+    ?>
+    
 </body>
+
 </html>
